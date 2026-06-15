a413269842565d2b783ebebf322499735669fc7d
apmabazar <shaerbaz61@gmail.com>
2026-06-15 08:34:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function showLogin() {
 
+    document.getElementById("loginForm").style.display = "block";
+    document.getElementById("signupForm").style.display = "none";
+
+    document.getElementById("loginTab")
+    .classList.add("active");
+
+    document.getElementById("signupTab")
+    .classList.remove("active");
+}
+
+function showSignup() {
+
+    document.getElementById("signupForm").style.display = "block";
+    document.getElementById("loginForm").style.display = "none";
+
+    document.getElementById("signupTab")
+    .classList.add("active");
+
+    document.getElementById("loginTab")
+    .classList.remove("active");
+}
